15a16e249647674be4f416634d63f087a3b8f377
MSI\sisi2 <zeal24360207@fsgcl.jp>
2025-07-01 09:43:39 +0900

    11.1 敵クラスの作成

---
+++ src/GameFrame.java
@@ -0,0 +1,55 @@
+import java.util.Vector;
+
+public class GameFrame extends MyFrame
+{
+	public void run() 
+	{
+		GameWorld.player=new Player(100,300,0,0);
+		addKeyListener(GameWorld.player);
+		GameWorld.playerBullets=new Vector<PlayerBullet>();
+		GameWorld.enemies=new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+		while(true)
+		{
+		
+		clear();
+			
+		GameWorld.player.draw(this);
+		GameWorld.player.move();
+		moveEnemies();
+	
+		for (int i=0; i<GameWorld.enemies.size();i++) 
+		{
+			Enemy e=GameWorld.enemies.get(i);
+			e.draw(this);
+			e.move();
+		}
+		int i=0;
+		while(i<GameWorld.playerBullets.size())
+		{
+			PlayerBullet b= GameWorld.playerBullets.get(i);
+			b.draw(this);
+			b.move();
+			if (b.y<0) {
+				GameWorld.playerBullets.remove(i);
+			}
+			else 
+			{
+			i++;
+			}
+		}
+		sleep(0.03);
+	    }
+	}
+
+	private void moveEnemies() {
+		for (int i=0; i<GameWorld.enemies.size();i++) 
+		{
+			Enemy e=GameWorld.enemies.get(i);
+			e.draw(this);
+			e.move();
+		}
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+}
